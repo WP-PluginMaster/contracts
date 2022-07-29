b85cc56cb9468a4e2a17e1d958eb4f4bfb260978
AL EMRAN <emrancu1@gmail.com>
2022-07-30 00:40:33 +0600

    Update SideMenuInterface.php

--- SideMenu/SideMenuInterface.php
+++ SideMenu/SideMenuInterface.php
@@ -1,12 +1,24 @@
 <?php
 
-
 namespace PluginMaster\Contracts\SideMenu;
-
 
 interface SideMenuInterface
 {
-    public static function parent(string $slug, array $options, callable $callback = null): void;
+    public static function parent(string $title, string $slug = null): self;
 
-    public static function child(string $slug, array $options): void;
+    public function capability(string $capability = 'manage_options'): self;
+
+    public function menuTitle(string $title): self;
+
+    public function callback($callback): self;
+
+    public function icon(string $icon): self;
+
+    public function position(int $position): self;
+
+    public function child(string $title, string $slug = null): self;
+
+    public static function submenu(string $parentSlug, string $title, string $slug = null): self;
+
+    public function getData(): array;
 }
