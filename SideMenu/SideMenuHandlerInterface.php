57aae1650b55e2344255cb42bcae122aa279df15
AL EMRAN <emrancu1@gmail.com>
2022-08-03 20:12:27 +0600

    Update SideMenu Interface

--- SideMenu/SideMenuHandlerInterface.php
+++ SideMenu/SideMenuHandlerInterface.php
@@ -14,13 +14,5 @@ interface SideMenuHandlerInterface
 
     public function loadMenuFile(string $sidemenu): self;
 
-    public function addMenuPage(string $slug, array $options): void;
-
-    public function registerParentMenu(array $options, string $slug): void;
-
     public function removeFirstSubMenu(): void;
-
-    public function addSubMenuPage(string $slug, array $options, string $parentSlug = ''): void;
-
-    public function validateOptions(array $options, bool $parent = true): void;
 }
